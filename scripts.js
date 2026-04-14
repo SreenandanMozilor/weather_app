de8fb9dd4c8bf099c9a09446439258073b051a6d
Sreenandan Pradeep <sreenandan@mozilor.com>
2026-04-14 09:50:27 +0530

    Func: getCoordintes() for geocoding

--- scripts.js
+++ scripts.js
@@ -1,10 +1,33 @@
 class WeatherError extends Error{
     constructor(message, type){
-        super(message, type)
+        super(message)
         this.type = type;
-        this.message = message;
         this.name = "WeatherError";
     }
+}
 
+async function getCoordinates(city) {
     
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1&language=en&format=json`;
+
+    try{
+        const response = await (fetch(url));
+        if(!response.ok){
+            throw new WeatherError("Failed to connect with Geocoding server", "GEOCODE_ERROR");
+        }
+
+        const data = await response.json(); // .json() also returns a promise
+
+        if(!data.results?.length){
+            throw new WeatherError("City not found", "NOT_FOUND");
+        }
+
+        const { latitude, longitude, name, country } = data.results[0];
+
+        return {latitude, longitude, name, country};
+
+    } catch(error) {
+        console.error("Geocoding failed: ", error.message);
+        throw error; //to let the code calling getCoordinates() know that an error occured, else it will think that the function succeeded with the result being undefined
+    }
 }
