9ddb9b281cc8f75a26e70f7bac05d7c036ffa6fd
Sreenandan Pradeep <sreenandan@mozilor.com>
2026-04-16 17:56:18 +0530

    Modified to include visibility property as well

--- backend/Api.js
+++ backend/Api.js
@@ -2,7 +2,7 @@ import { WeAppError } from "./Error";
 
 async function getCoordinates(cityName) {
     
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(cityName)}&count=1&language=en&format=json`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,weather_code,visibility&daily=temperature_2m_max,temperature_2m_min&timezone=auto`;
 
     try {
         const response = await fetch(url);
@@ -58,7 +58,8 @@ async function getWeather(cityName) {
                 date,
                 max: temperature_2m_max[index],
                 min: temperature_2m_min[index]
-            })).slice(0,5) ?? [] // nullish coalescing operator
+            })).slice(0,5) ?? [], // nullish coalescing operator
+            visibility: (data.current.visibility / 1000).toFixed(1), // Convert meters to km
         }
 
     } catch (error){
